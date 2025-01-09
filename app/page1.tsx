e66bb8ee20503255c54b5db71e00e6502f80b1c1
ShrineSigdel <123295702+ShrineSigdel@users.noreply.github.com>
2025-01-09 22:35:01 +0545

    Finished page1.tsx

--- app/page1.tsx
+++ app/page1.tsx
@@ -1,52 +1,104 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
-import { PaperProvider } from 'react-native-paper';
-import { List } from 'react-native-paper';
+import { View, Text, ScrollView, StyleSheet } from 'react-native';
+import { MD3LightTheme as DefaultTheme, PaperProvider } from 'react-native-paper';
+import { List, Button } from 'react-native-paper';
+import { Image } from 'expo-image'
 
 const Page1: React.FC = () => {
     const [expanded, setExpanded] = React.useState(true);
+    const heroImage = require('../assets/images/hero1.png');
+    const theme = {
+        ...DefaultTheme,
+        // Specify custom property in nested object
+
+        colors: {
+            ...DefaultTheme.colors,
+            primary: '#198BEF',
+            secondary: '#070D81',
+            text: '#000000',
+            background: '#FFFFFF',
+        },
+    };
+
 
     const handlePress = () => setExpanded(!expanded);
     return (
-        <PaperProvider>
-            <View style={styles.container}>
-                <Text style={styles.title}>Welcome to the Onboarding Page 1</Text>
-                <Text style={styles.paragraph}>This is the first step of the onboarding process.</Text>
-            </View>
-            <List.Section title="Accordions">
-                <List.Accordion
-                    title="Uncontrolled Accordion"
-                    left={props => <List.Icon {...props} icon="folder" />}>
-                    <List.Item title="First item" />
-                    <List.Item title="Second item" />
-                </List.Accordion>
-
-                <List.Accordion
-                    title="Controlled Accordion"
-                    left={props => <List.Icon {...props} icon="folder" />}
-                    expanded={expanded}
-                    onPress={handlePress}>
-                    <List.Item title="First item" />
-                    <List.Item title="Second item" />
-                </List.Accordion>
-            </List.Section>
+        <PaperProvider theme={theme}>
+            <ScrollView className='flex-col content-center h-full bg-white py-4 mb-4'>
+                <View className='flex-col justify-around content-center h-52 px-4'>
+                    <Text className='font-bold text-2xl text-center '>Hi There!</Text>
+                    <Text className='font-light text-lg text-center'>Tell us about yourself, to help us build your profile</Text>
+                    <Text className='font-bold text-lg text-center'>Tell us which best describes your impairment(s)</Text>
+                </View>
+                <Image source={heroImage} contentFit='cover' className='h-[220px] w-auto'></Image>
+                <List.Section>
+                    <List.Accordion
+                        title="Upper limb - Prothesesis"
+                        theme={{
+                            ...theme,
+                            colors: {
+                                ...theme.colors,
+                            },
+                        }}
+                        titleStyle={{ fontWeight: 'bold', fontSize: 16 }}
+                        onPress={handlePress}>
+                        <List.Item title="Single" titleStyle={styles.title} />
+                        <List.Item title="Double" titleStyle={styles.title} />
+                    </List.Accordion>
+                    <List.Accordion
+                        title="Lower limb - Prothesesis"
+                        theme={{
+                            ...theme,
+                            colors: {
+                                ...theme.colors,
+                            },
+                        }}
+                        titleStyle={{ fontWeight: 'bold', fontSize: 16 }}
+                        onPress={handlePress}>
+                        <List.Item title="Single" titleStyle={styles.title} />
+                        <List.Item title="Double" titleStyle={styles.title} />
+                    </List.Accordion>
+                    <List.Accordion
+                        title="Upper limb - Amputee"
+                        theme={{
+                            ...theme,
+                            colors: {
+                                ...theme.colors,
+                            },
+                        }}
+                        titleStyle={{ fontWeight: 'bold', fontSize: 16 }}
+                        onPress={handlePress}>
+                        <List.Item title="Single" titleStyle={styles.title} />
+                        <List.Item title="Double" titleStyle={styles.title} />
+                    </List.Accordion>
+                    <List.Accordion
+                        title="Lower limb - Amputee"
+                        theme={{
+                            ...theme,
+                            colors: {
+                                ...theme.colors,
+                            },
+                        }}
+                        titleStyle={{ fontWeight: 'bold', fontSize: 16 }}
+                        onPress={handlePress}>
+                        <List.Item title="Single" titleStyle={styles.title} />
+                        <List.Item title="Double" titleStyle={styles.title} />
+                    </List.Accordion>
+                </List.Section>
+                <Button mode="contained" onPress={() => console.log('Pressed')} style={{ margin: 20, backgroundColor: '#198BEF' }}>
+                    Continue
+                </Button>
+            </ScrollView>
         </PaperProvider>
     );
 };
 
+
+
+
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-        padding: 16,
-    },
     title: {
-        fontSize: 24,
-        fontWeight: 'bold',
-        marginBottom: 16,
-    },
-    paragraph: {
+        color: '#198BEF',
         fontSize: 16,
         textAlign: 'center',
     },
