90fd74325a2e557622fd6ce4928cbfde782fb753
nishanadhikari1 <adhikarin537@gmail.com>
2025-01-11 14:28:08 +0545

    ui update

--- app/page3.tsx
+++ app/page3.tsx
@@ -41,8 +41,6 @@ const Page3: React.FC = () => {
                         <TouchableOpacity
                             key={index}
                             onPress={() => handleOptionPress(option)}
-                            className={`flex flex-col items-center justify-center p-5 h-20 mx-10 mb-5 border-2 rounded-[20px] ${isSelected ? 'border-[#198BEF]' : 'border-[#D9D9D9]'
-                                }`}
                             className={`flex flex-row items-center justify-between p-3 h-16 mx-10 mb-3 border-2 rounded-[20px] ${
                                 isSelected ? 'border-[#198BEF] bg-[#E6F4FF]' : 'border-[#D9D9D9] bg-[#F9FAFB]'
                             }`}
